1294b3615aeb3a8f114b998e196394d54ba82788
olivas1406 <olivas1406@msn.com>
2018-05-02 01:35:52 -0700

    app setup complete, db setup complete, working on config setup

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+
+INSERT INTO burgers (burger_name) VALUES ("Double Double");
+
+INSERT INTO burgers (burger_name) VALUES ("4x4");
