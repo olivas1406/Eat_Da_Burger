3d70b9132ff4ecf81160c53abeb4e36e2f04c5fc
olivas1406 <olivas1406@msn.com>
2018-05-06 22:58:26 -0700

    update burger.js

--- models/burger.js
+++ models/burger.js
@@ -4,25 +4,21 @@ var orm = require("../config/orm.js")
 var burger = {
     selectAll: function(callback) {
         orm.selectAll("burgers", function(res) {
-         //   console.log(res);                                 // REMOVE ME REMOVE ME REMOVE ME
             callback(res)
         });
     },
     insertOne: function(column, value, callback) {
         orm.insertOne("burgers", column, value, function(res) {
-        //    console.log(res);                                 // REMOVE ME REMOVE ME REMOVE ME
             callback(res);
         });
     },
     updateOne: function(colVals, condition, callback) {
         orm.updateOne("burgers", colVals, condition, function(res) {
-       //  console.log(res);                                   // REMOVE ME REMOVE ME REMOVE ME
             callback(res);
         });
     },
     deleteOne: function(value, callback) {
         orm.deleteOne("burgers", "id", value, function(res) {
-        // console.log(res);                                    // REMOVE ME REMOVE ME REMOVE ME
             callback(res);
         });
     }
