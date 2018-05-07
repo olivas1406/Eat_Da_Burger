8afb4564349d97558cebb64c70d9960e8aa85734
olivas1406 <olivas1406@msn.com>
2018-05-06 18:58:24 -0700

    updated Title on main.handlebars and updated comments on burger.js and orm.js

--- config/orm.js
+++ config/orm.js
@@ -6,7 +6,7 @@ var orm = {
      var queryString = "SELECT * FROM " + what + ";";
       connection.query(queryString, [what], function(err, res) {
         if (err) throw err;
-        console.log(res);
+     //   console.log(res);                                     REMOVE ME REMOVE ME REMOVE ME
         callback(res);   
       });
     },
@@ -19,11 +19,11 @@ var orm = {
     },
     updateOne: function(table, values, id) {
         var queryString = "INSERT INTO ?? VALUES ?? WHERE ID = ??";
-        //INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`) VALUES ('5', 'Steak Burger');
+        //INSERT INTO `burgers_db`.`burgers` (`id`, `burger_name`) VALUES ('5', 'Steak Burger');  // <-- Actual SQL Commands - need to convert
 
         connection.query(queryString, [table, values, id], function(err, res) {
             if (err) throw err;
-            console.log(res);
+       //     console.log(res);                             REMOVE ME REMOVE ME REMOVE ME
         });
     },
     deleteOne: function(table, values1, values2, callback) {
@@ -31,7 +31,7 @@ var orm = {
 
         connection.query(queryString, [values2], function(err, res) {
             if (err) throw err;
-            console.log(res);
+         //   console.log(res);                             REMOVE ME REMOVE ME REMOVE ME
             callback(res);
         });
     }
--- models/burger.js
+++ models/burger.js
@@ -4,23 +4,25 @@ var orm = require("../config/orm.js")
 var burger = {
     selectAll: function(callback) {
         orm.selectAll("burgers", function(res) {
-            console.log(res);
+         //   console.log(res);                                 // REMOVE ME REMOVE ME REMOVE ME
             callback(res)
         });
     },
     insertOne: function(table, values, callback) {
         orm.insertOne("burgers", table, values, function(res) {
-            console.log(res);
+        //    console.log(res);                                 // REMOVE ME REMOVE ME REMOVE ME
             callback(res);
         });
     },
     updateOne: function(column, value, callback) {
         orm.updateOne("burgers", "devoured", column, "id", value, function(res) {
+        //  console.log(res);                                   // REMOVE ME REMOVE ME REMOVE ME
             callback(res);
         });
     },
     deleteOne: function(value, callback) {
         orm.deleteOne("burgers", "id", value, function(res) {
+        // console.log(res);                                    // REMOVE ME REMOVE ME REMOVE ME
             callback(res);
         });
     }
