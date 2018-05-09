5e04b9b4fd34aa7ef824e4e7dd261497b6603d7a
olivas1406 <olivas1406@msn.com>
2018-05-08 23:26:29 -0700

    updated CSS

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,7 @@ INSERT INTO burgers (burger_name) VALUES ("Double Double");
 INSERT INTO burgers (burger_name) VALUES ("4x4 Burger");
 
 INSERT INTO burgers (burger_name) VALUES ("6x6 Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("8 Hamburger Patties - no bun");
+
+INSERT INTO burgers (burger_name) VALUES ("Steak Burger");
