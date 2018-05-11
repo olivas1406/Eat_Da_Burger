499e6379fdf1326110d21c47d8701affc694e3ef
olivas1406 <olivas1406@msn.com>
2018-05-10 18:42:45 -0700

    updated for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,24 @@
 var mysql = require("mysql");             
-
+/*
 var connection = mysql.createConnection({ // Set connection parameters
   host: "localhost",
   user: "root",
   password: "tiMh9AwUOkKzsfX1wVBs",
   database: "burgers_db"
 });
+*/
+
+var connection;
+if(process.env.JAWSDB_URL) {            //For Heroku Deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({ // Set local connection parameters
+    host: "localhost",
+    user: "root",
+    password: "tiMh9AwUOkKzsfX1wVBs",
+    database: "burgers_db"
+    });
+};
 
 connection.connect(function(err) {      // Create connection
   if (err) {
